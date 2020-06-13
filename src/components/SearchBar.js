5ba4b0e36385bd93cc673e71a251c4b83f15b210
Victor Longon <victor.longon@etraveligroup.com>
2020-06-14 01:28:18 +0200

    Scafold search request

---
+++ src/components/SearchBar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Feather } from '@expo/vector-icons';
+import { View, TextInput } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+const styles = EStyleSheet.create({
+  background: {
+    backgroundColor: '#E1E1E1',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    marginTop: 10,
+    flexDirection: 'row',
+  },
+  icon: { fontSize: 34, alignSelf: 'center', marginHorizontal: 14 },
+  input: { flex: 1, fontSize: 18 },
+});
+
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+  return (
+    <View style={styles.background}>
+      <Feather name="search" style={styles.icon} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.input}
+        placeholder="Search"
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
+import { useQuery } from 'react-query';
+import SearchBar from '../components/SearchBar';
+import api from '../utils/api';
 
-const Search = () => (
-  <View>
-    <Text>Search Screen</Text>
-  </View>
-);
+const Content = ({ error, data, status, isFetching }) => {
+  if (status === 'loading' || isFetching) {
+    return <Text>Loading...</Text>;
+  }
+  if (status === 'error') {
+    return <Text>Error: {error.message}</Text>;
+  }
+
+  const businesses = data?.data?.businesses || [];
+  return (
+    <Text>{businesses.length ? `we have found: ${businesses.length}` : 'No results yet'}</Text>
+  );
+};
+
+const Search = () => {
+  const [term, setTerm] = useState('');
+  const { status, data, error, refetch, isFetching } = useQuery(
+    'searchResults',
+    () => api.get('/search', { params: { term, limit: 50, location: 'gothenburg' } }),
+    { manual: true, initialData: [] },
+  );
+
+  return (
+    <View>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={refetch} />
+      <Content status={status} error={error} data={data} isFetching={isFetching} />
+    </View>
+  );
+};
 
 export default Search;
