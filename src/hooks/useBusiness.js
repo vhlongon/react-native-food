e098c0fdcab1b5ac63556d5231798506b4f339ae
Victor Longon <victor.longon@etraveligroup.com>
2020-06-14 23:14:09 +0200

    refactor custom hooks

--- src/hooks/useBusiness.js
+++ src/hooks/useBusiness.js
@@ -1,16 +1,11 @@
 import { useQuery } from 'react-query';
 import api from '../utils/api';
 
-const getBusiness = async (_, { id }) => {
+const getBusinessById = async (_, id) => {
   const { data } = await api.get(`/${id}`);
   return data;
 };
 
-const useBusiness = ({ variables: { id }, initialData = {}, retry = false, ...config } = {}) =>
-  useQuery(['searchResults', { id }], getBusiness, {
-    initialData,
-    retry,
-    ...config,
-  });
+const useBusiness = id => useQuery(['business', id], getBusinessById);
 
 export default useBusiness;
--- src/hooks/useBusinesses.js
+++ src/hooks/useBusinesses.js
@@ -1,26 +1,28 @@
+import { CancelToken } from 'axios';
 import { useQuery } from 'react-query';
 import api from '../utils/api';
 
-const getBusinesses = async (_, variables) => {
-  const { data } = await api.get('/search', { params: variables });
-  return data.businesses;
+const getBusinesses = async (_, params) => {
+  // Create a new CancelToken source for this request
+  const source = CancelToken.source();
+  const promise = api.get('/search', {
+    // Pass the source token to your request
+    cancelToken: source.token,
+    params,
+  });
+  // Cancel the request if React Query calls the `promise.cancel` method
+  promise.cancel = () => {
+    source.cancel('Query was cancelled by React Query');
+  };
+
+  return promise;
 };
 
 const useBusinesses = ({
-  variables: { term = 'pasta', limit = 50, location = 'gothenburg' },
+  variables: { term = 'pasta', limit = 50, location = 'gothenburg' } = {},
   initialTerm,
-  manual = true,
-  initialData = [],
-  retry = false,
-  ...config
-} = {}) => {
-  const termToSearch = term || initialTerm;
-  return useQuery(['searchResults', { term: termToSearch, limit, location }], getBusinesses, {
-    manual,
-    initialData,
-    retry,
-    ...config,
-  });
-};
+  config,
+} = {}) =>
+  useQuery(['businesses', { term: term || initialTerm, limit, location }], getBusinesses, config);
 
 export default useBusinesses;
