e5c0ab4f91b2413e5249d4056a8a06b38300bf59
Victor Longon <victor.longon@etraveligroup.com>
2020-06-14 23:13:18 +0200

    change yelp url if browsers to work with cors

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,17 @@
 import axios from 'axios';
+import Constants from 'expo-constants';
 import { API_KEY } from '../../apiConfig';
 
+const browsers = ['Chrome', 'Firefox'];
+const { deviceName } = Constants;
+
+const rootUrl = 'https://api.yelp.com/v3/businesses';
+const baseURL = `${
+  browsers.includes(deviceName) ? 'https://cors-anywhere.herokuapp.com/' : ''
+}${rootUrl}`;
+
 export default axios.create({
-  baseURL: 'https://api.yelp.com/v3/businesses',
+  baseURL,
   headers: {
     Authorization: `Bearer ${API_KEY}`,
   },
