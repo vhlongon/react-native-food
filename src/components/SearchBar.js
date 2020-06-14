8ed0d2b4f4dc872a1e50d54faa2cc115166a797e
Victor Longon <victor.longon@etraveligroup.com>
2020-06-14 23:13:51 +0200

    add refetch button to searchbar and support to trigger search via key press

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Feather } from '@expo/vector-icons';
+import { Feather, SimpleLineIcons } from '@expo/vector-icons';
 import { View, TextInput } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
@@ -17,19 +17,29 @@ const styles = EStyleSheet.create({
   input: { flex: 1, fontSize: 18 },
 });
 
-const SearchBar = ({ term, onTermChange, onTermSubmit }) => (
-  <View style={styles.background}>
-    <Feather name="search" style={styles.icon} />
-    <TextInput
-      autoCapitalize="none"
-      autoCorrect={false}
-      style={styles.input}
-      placeholder="Search"
-      value={term}
-      onChangeText={onTermChange}
-      onEndEditing={onTermSubmit}
-    />
-  </View>
-);
+const SearchBar = ({ term, onTermChange, onTermSubmit, refetch }) => {
+  const handleKeyPress = ({ key }) => {
+    if (key === 'Enter') {
+      refetch();
+    }
+  };
+
+  return (
+    <View style={styles.background}>
+      <Feather name="search" style={styles.icon} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.input}
+        placeholder="Search"
+        value={term}
+        onKeyPress={handleKeyPress}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
+      <SimpleLineIcons name="reload" style={styles.icon} onPress={refetch} />
+    </View>
+  );
+};
 
 export default SearchBar;
